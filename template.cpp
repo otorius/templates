38d618c1546d2514e69f7ba88ce6922bd37d6b54
otarius <141804089+otarinio@users.noreply.github.com>
2024-07-17 15:07:37 +0400

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <bits/extc++.h>
+using namespace __gnu_pbds;
+using namespace std;
+
+// #pragma GCC optimize("Ofast")
+// #pragma GCC optimize ("unroll-loops")
+// #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+#define ff first
+#define sc second
+#define pb push_back
+#define ll long long
+#define pll pair<ll, ll>
+#define pii pair<int, int>
+#define ull unsigned long long
+#define all(x) (x).begin(),(x).end()
+
+// #define int long long
+// #define int unsigned long long
+
+// #define ordered_set(T) tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>
+// #define ordered_multiset(T) tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>
+
+void open_file(string filename) {
+    freopen((filename + ".in").c_str(), "r", stdin);
+    freopen((filename + ".out").c_str(), "w", stdout);
+}
+
+// const ll mod = 1e9 + 7;
+// const ll mod = 998244353;
+
+const ll inf = 1e9;
+const ll biginf = 1e18;
+// const int maxN;
+
+void solve() {
+    
+}
+int32_t main() { 
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+        cout << '\n';
+    }
+    return 0;
+}
