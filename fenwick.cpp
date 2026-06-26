79ecf954dd1d61a8d316a0066735ec1c135ea6c4
otarius <141804089+otorius@users.noreply.github.com>
2026-06-26 14:47:56 +0400

    Update fenwick.cpp
    Signed-off-by: otarius <141804089+otorius@users.noreply.github.com>

--- fenwick.cpp
+++ fenwick.cpp
@@ -1,25 +1,23 @@
-// #define fenint int
-// #define fenint long long
-// #define fenint unsigned long long
-
 struct fenwick {
-    fenint bit[maxN];
-    
-    void add(int x, fenint d) {
-        for ( ; x < maxN; x += (x & -x)) bit[x] += d;
+    int n; vector<ll> bit;
+
+    fenwick(int _n) : n(_n), bit(n + 1, 0) {}
+
+    void add(int x, ll d) {
+        for ( ; x <= n; x += (x & -x)) bit[x] += d;
     }
-    
-    fenint get(int x) {
-        fenint ans = 0;
+
+    ll get(int x) {
+        ll ans = 0;
         for ( ; x; x -= (x & -x)) ans += bit[x];
         return ans;
     }
-    
-    fenint query(int l, int r) {
+
+    ll query(int l, int r) {
         return get(r) - get(l - 1);
     }
-    
+
     void init() {
-        for (int i = 0; i < maxN; i++) bit[i] = 0;
+        fill(bit.begin(), bit.end(), 0);
     }
 };
